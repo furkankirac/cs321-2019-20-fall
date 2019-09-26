5b8be6f0df9ed70d4fb37c238637756992084cb2
Furkan Kıraç <fkirac@gmail.com>
2019-09-26 12:46:38 +0300

    First commit. C and C++ versions of Hello World program.

---
+++ week1-app1.cpp
@@ -0,0 +1,32 @@
+// OOP and FP paradigms
+// Compile-time vs run-time parameters
+// Function objects, lambda functions
+// OOP interfaces, casting, copy/move construction
+
+// Midterm Exam 1 (25%)
+// Functional programming
+// Varadic functions, variadic templates
+// Varadic functions, variadic templates
+
+// Midterm Exam 2 (35%)
+// Static polymorphism
+// Compile-time programming
+// Continuation monads
+// Summary
+// OOP and FP paradigms
+// Compile-time vs run-time parameters
+
+// Final Exam (40%)
+
+// Fridays are Lab Sections
+
+// Will use:
+//   Compiler: Clang 8+ and C++17 standard. Sometimes we'll sneak into C++20 (called C++2a for now).
+//   Editor: Qt Creator 4.10+. Lightweight, directly supports CMake
+
+// COMPILER CONFORMANCE: https://en.cppreference.com/w/cpp/compiler_support
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ week1-app2.cpp
@@ -0,0 +1,9 @@
+//#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+//    printf("Hello World\n");
+    std::cout << "Hello World in C++" << std::endl;
+    return 0;
+}
