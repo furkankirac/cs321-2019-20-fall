13ffbbe96491a0ecf6d8678c828603095b68c081
Furkan Kıraç <fkirac@gmail.com>
2019-10-03 12:50:08 +0300

    OOP style of doing things.

--- week2-app1.cpp
+++ week2-app1.cpp
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+//#include <string.h>
 #include <iostream>
 
 //TYPE varname;
@@ -10,31 +10,62 @@ int i;
 //    char name[20];
 //} Employee;
 
+
 struct Employee
 {
+    static int num_employee;
+
     float salary;
     unsigned char age;
-    char name[20];
+    char* name;
+
+    Employee(float salary_, unsigned char age_, const char* name_) :
+        salary(salary_), age(age_)
+    {
+        int sz = strlen(name_);
+        name = new char[sz+1];
+        strcpy(name, name_);
+        num_employee++;
+    }
+
+    ~Employee()
+    {
+        delete[] name;
+        num_employee--;
+    }
+
+    void print()
+    {
+    //    this->salary,   this[0].salary,   (*this).salary
+//        std::cout << "Salary=" << this->salary << std::endl;
+//        std::cout << "Age=" << (int)this->age << std::endl;
+//        std::cout << "Name=" << this->name << std::endl;
+        std::cout << "Salary=" << salary << std::endl;
+        std::cout << "Age=" << (int)age << std::endl;
+        std::cout << "Name=" << name << std::endl;
+    }
+
 };
 
-void printEmployee(Employee* ptr)
+int Employee::num_employee = 0;
+
+
+void STRCPY(char* dest, const char* src)
 {
-//    ptr->salary,   ptr[0].salary,   (*ptr).salary
-    std::cout << "Salary=" << ptr->salary << std::endl;
-    std::cout << "Age=" << (int)ptr->age << std::endl;
+    while(*src != '\0')
+        *dest++ = *src++;
+    *dest = '\0';
 }
 
 int main(int argc, char* argv[])
 {
-    Employee e1, e2, e3;
+    Employee e1(10300.2f, 42, "Furkan");
+    Employee e2(5000.0f, 30, "Hasan");
 
-    e1.age = 42;
-    e1.salary = 10300.2f;
-//    employee.name = "Furkan";
-    e2.age = 30;
-    e2.salary = 5000.0f;
-
-    printEmployee(&e1);
+//    Employee* e1_ptr = &e1;
+//    e1_ptr->print();
+    e1.print();
+//    printEmployee(&e1);
 
 //    printf("Hello World\n");
     std::cout << "Hello World in C++" << std::endl;
